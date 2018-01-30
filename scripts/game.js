8ee6b64119d6d6a2fa9a818b814d1cbbd94d1734
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-01-30 09:46:33 -0700

    fixed navbar, starting narrative project

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,21 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+function draw() {
+
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    // document.write(document.getElementById('navbar').offsetHeight);
+    console.log(ctx.canvas.width + ":" + ctx.canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw, 10);
